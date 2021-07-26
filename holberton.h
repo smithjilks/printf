f9503ef55cb564fec05252b24bf1be699a5b9b38
smithjilks <smithjilks@gmail.com>
2021-07-26 22:52:52 +0300

    Update holberton.h

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_str(char *c);
+int print_int(int num);
+int print_Str(char *c);
+int print_bin(int num);
+int print_hex(int num);
+int print_hexMin(int num);
+int print_oct(int num);
+int print_unt(int num);
+#endif
